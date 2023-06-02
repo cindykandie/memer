488ce004934f253a739f1c8b286cf6e4fe326b3a
cindykandie <cindyjk6@gmail.com>
2023-06-03 00:40:14 +0300

    Create meme generator

---
+++ README.md
@@ -0,0 +1,54 @@
+# Memer 😄
+
+Memer is a web application that lets you unleash your creativity and create hilarious meme statements. 🎉 It provides an easy-to-use interface where you can enter your text and download the meme as an image. 😁 Memer also offers a customizable theme with light and dark modes. 🌞🌚
+
+## Features 🚀
+
+- Enter your funniest meme statements and phrases 📝
+- Download your creations as shareable images 🖼️
+- Switch between a light and dark theme to match your mood 😎🌙
+
+## Demo 🌐
+
+You can check out the live demo of Memer [here](https://example.com). 🎈
+
+## Screenshots 📷
+
+![Screenshot 1](screenshots/screenshot1.png)
+![Screenshot 2](screenshots/screenshot2.png)
+
+## Installation ⚙️
+
+1. Clone the repository: `git clone https://github.com/your-username/memer.git`
+2. Navigate to the project directory: `cd memer`
+3. Open `index.html` in your favorite web browser.
+
+## Usage 🎯
+
+1. Enter your wittiest meme statements in the provided textarea. 😄
+2. Click the "Done" button to generate your hilarious meme. 🚀
+3. If the textarea is not empty, a dialog box will pop up asking for confirmation. 📢
+4. Click "Yes" if you're ready to witness the power of your meme creation! 😎
+5. Click "No" if you want to go back and make changes to your meme statement. 🔄
+6. To match your meme-making mood, click the theme button (sun/moon icon) in the header section to toggle between the light and dark themes. ☀️🌒
+
+## License 📜
+
+This project is licensed under the [MIT License](LICENSE). Feel free to use and modify the code for your own meme-making adventures! 😄
+
+## Acknowledgements 🙌
+
+- [Open Source License](https://opensource.org/licenses/MIT) 📝
+- [Iconfinder](https://www.iconfinder.com/) for the awesome sun and moon icons ☀️🌙
+
+## Got a Joke? 😄
+
+Why did the computer go to art school? 🎨
+
+Because it wanted to become a graphic designer! 😄🖥️
+
+Why don't scientists trust atoms? ⚛️
+
+Because they make up everything! 🤣
+
+Have fun memeing and enjoy spreading laughter with Memer! 😄🎉🚀
---
+++ main.js
@@ -0,0 +1,57 @@
+const themeButton = document.getElementById('themeButton');
+const themeIcon = document.getElementById('themeIcon');
+const headerText = document.getElementById('headerText');
+const textarea = document.getElementById('memeTextarea');
+const modal = document.getElementById('modal');
+const modalText = document.getElementById('modalText');
+const doneButton = document.getElementById('doneButton');
+
+themeButton.addEventListener('click', toggleTheme);
+
+function toggleTheme() {
+    document.body.classList.toggle('light-theme');
+    themeIcon.src = document.body.classList.contains('light-theme') ? 'assets/moon.svg' : 'assets/sun.svg';
+    headerText.textContent = document.body.classList.contains('light-theme') ? 'Welcome to this Statement meme site (Light Mode)' : 'Welcome to this Statement meme site (Dark Mode)';
+}
+
+doneButton.addEventListener('click', () => {
+    const text = textarea.value.trim();
+    if (text !== '') {
+        const confirmDialog = confirm(`Are you sure? Text: "${text}"`);
+        if (confirmDialog) {
+            modalText.textContent = text;
+            modal.style.display = 'block';
+        } else {
+            textarea.focus();
+        }
+    }
+});
+
+modal.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+function downloadModalImage() {
+    const canvas = document.createElement('canvas');
+    const modalContent = document.querySelector('.modal-content');
+
+    canvas.width = modalContent.offsetWidth;
+    canvas.height = modalContent.offsetHeight;
+
+    const context = canvas.getContext('2d');
+    context.fillStyle = '#000000';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    const modalText = document.getElementById('modalText');
+    const text = modalText.textContent;
+    context.font = '24px Arial';
+    context.fillStyle = '#ffffff';
+    context.textAlign = 'center';
+    context.fillText(text, canvas.width / 2, canvas.height / 2);
+
+    const link = document.createElement('a');
+    link.download = 'modal.png';
+    link.href = canvas.toDataURL();
+    link.click();
+}
+
