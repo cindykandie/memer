e8a65859bdca36818142aca85a305130ab10a035
cindykandie <cindyjk6@gmail.com>
2023-06-05 11:40:20 +0300

    Wrap my text in the downloaded image

--- main.js
+++ main.js
@@ -37,9 +37,9 @@ function downloadModalImage() {
     const isLightTheme = document.body.classList.contains('light-theme');
     const backgroundColor = isLightTheme ? '#ffffff' : '#000000';
     const textColor = isLightTheme ? '#000000' : '#ffffff';
-    const maxWidth = 270 // Maximum width in pixels
+    const maxWidth = 280 // Maximum width in pixels
     const maxHeight = 500; // Maximum height in pixels
-    const padding = 20; // Padding in pixels
+    const padding = 10; // Padding in pixels
   
     canvas.width = modalContent.offsetWidth;
     canvas.height = modalContent.offsetHeight;
